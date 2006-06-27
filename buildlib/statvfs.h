f312c597776c8472acd80b7ce7980d14c6aa14ca
pmatilai <pmatilai>
2006-06-27 16:00:59 +0000

    - fix a minor bug in configure.ac (Ralf)

--- buildlib/statvfs.h
+++ buildlib/statvfs.h
@@ -1,11 +1,11 @@
 /* Compatibility for systems with out Single Unix Spec statvfs */
 #include <config.h>
 
-#ifdef HAVE_VFS_H
+#ifdef HAVE_SYS_VFS_H
 #include <sys/vfs.h>
 #endif
 
-#ifdef HAVE_MOUNT_H
+#ifdef HAVE_SYS_MOUNT_H
 #include <sys/param.h>
 #include <sys/mount.h>
 #endif
